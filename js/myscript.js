f0c4a7f9c35e7f3a7a4de7d98e46344838d1615d
Alessandro <luisialessandro95@gmail.com>
2021-11-12 18:23:53 +0100

    col-sx-js-parte1

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,44 @@
 // alert('Ciaone');
 
+const itemsArrey = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+
+// const titleArrey = [
+//     'Svezia',
+//     'Svizzera',
+//     'Gran Bretagna',
+//     'Germania',
+//     'Paradise'
+// ]
+
+// const textArrey = [
+//     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+//     'Lorem ipsum',
+//     'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+//     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+//     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+// ]
+
+let items ='';
+
+for (let i = 0; i < itemsArrey.length; i++){
+
+    items += `
+        <div class="col-sx-img-box">          
+            <img src="${itemsArrey[i]}" alt="">
+        </div>
+    ` 
+}
+
+console.log(items);
+
+const colSx = document.querySelector('.col-sx');
+
+colSx.innerHTML = items;
+
 
